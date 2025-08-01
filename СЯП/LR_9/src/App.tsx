6b3ddaf2ff1ddf1165d634d8795449447e532e24
Stanislav Yarochovich <histor.245@gmail.com>
2025-08-01 14:18:20 +0300

    Add files via upload

---
+++ СЯП/LR_9/src/App.tsx
@@ -0,0 +1,21 @@
+import classes from './App.module.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MainComponent from './Components/MainComponent/MainComponent';
+
+function App() {
+
+  return (
+    <div className={classes.App}>
+      <BrowserRouter basename='/TodoList-React-TS'>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<MainComponent />} />
+            <Route path='*' element={<h1>Not found page</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
